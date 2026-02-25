1520671b2b837f1a61d7632b4420ca38d607f186
Doot1029 <96122758+Doot1029@users.noreply.github.com>
2026-02-24 23:04:25 -0900

    "Fix"

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const LORE = require("./node_modules/lorejs/lore.js");
 const game = new LORE.Game();
 
 // Load the game
-game.loadNovel("./node_modules/lorejs/games/lemegeton.js");
-
-// Start the game
-game.start();
+game.loadNovel("./node_modules/lorejs/games/lemegeton.js").then(() => {
+    // Game starts automatically in loadNovel
+});
