a09d8fac98c0f0f02a3d1147a2a5e65dca97f184
Doot1029 <96122758+Doot1029@users.noreply.github.com>
2026-02-26 16:59:41 -0900

    "responsive_ui_update"

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ const LORE = require("./node_modules/lorejs/lore.js");
 const game = new LORE.Game();
 
 // Load the game
-game.loadNovel("./node_modules/lorejs/games/lemegeton.js").then(() => {
-    // Game starts automatically in loadNovel
-});
+game.loadNovel("./node_modules/lorejs/games/lemegeton.js");
